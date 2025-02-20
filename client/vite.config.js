825004e4f3e5c3b10db70b9200a6b76ea60b95fc
Maryann Mweru <mwerumaryann@gmail.com>
2025-02-21 00:24:46 +0300

    feat: Update on vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/', // Add this line
@@ -15,43 +14,5 @@ export default defineConfig({
         secure: false,
       }
     }
-  },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt'],
-      manifest: {
-        name: 'Welzyne Courier Services',
-        short_name: 'Welzyne',
-        start_url: '/',
-        display: 'standalone',
-        theme_color: '#000000',
-        background_color: '#ffffff',
-        icons: [
-          {
-            src: '/icons/192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icons/512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/your-api\.com\/.*$/, // Cache API requests
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-            },
-          },
-        ],
-      },
-    }),
-  ],
+  }
 });
