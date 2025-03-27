625b0cb20ad763280e16866eeec016107300f896
Maryann Mweru <mwerumaryann@gmail.com>
2025-03-28 02:54:59 +0300

    feat: Add of the X icon

--- client/src/CustomIcons.jsx
+++ client/src/CustomIcons.jsx
@@ -69,7 +69,6 @@ export const Search = ({ size }) => (
   </IconWrapper>
 );
 
-// Added Camera icon
 export const Camera = ({ size }) => (
   <IconWrapper size={size}>
     <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
@@ -77,7 +76,6 @@ export const Camera = ({ size }) => (
   </IconWrapper>
 );
 
-// Added CheckCircle icon
 export const CheckCircle = ({ size }) => (
   <IconWrapper size={size}>
     <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
@@ -85,7 +83,6 @@ export const CheckCircle = ({ size }) => (
   </IconWrapper>
 );
 
-// New Icons for Admin Dashboard
 export const Plus = ({ size }) => (
   <IconWrapper size={size}>
     <line x1="12" y1="5" x2="12" y2="19" />
@@ -140,6 +137,14 @@ export const List = ({ size }) => (
   </IconWrapper>
 );
 
+// Add the X (close) icon here
+export const X = ({ size }) => (
+  <IconWrapper size={size}>
+    <line x1="18" y1="6" x2="6" y2="18" />
+    <line x1="6" y1="6" x2="18" y2="18" />
+  </IconWrapper>
+);
+
 export default {
   Package,
   Truck,
@@ -156,5 +161,6 @@ export default {
   Mail,
   Phone,
   Dashboard,
-  List
+  List,
+  X // Make sure to add X to the default export
 };
